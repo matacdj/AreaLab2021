2ead7f638203d924d5049f7c03758f395bc77d92
Matteo Accordini <matteoaccordini18@gmail.com>
2021-04-19 19:27:40 +0200

    creazioni variabili in js

--- HTML5GiocoDelNoveAreaLab/public_html/js/app.js
+++ HTML5GiocoDelNoveAreaLab/public_html/js/app.js
@@ -1,7 +1,19 @@
-
-
 /* Dizionario Applicazione ad evnti in Javascript */
 var APP={
+    
+    /* Variabili globali */
+    
+    listaGioco : [],
+    dizPosizioniVicine : {0:[1,3], 
+                          1:[0,2,4],
+                          2:[1,5], 
+                          3:[0,4,6], 
+                          4:[1,3,5,7], 
+                          5:[2,4,8], 
+                          6:[3,7], 
+                          7:[4,6,8], 
+                          8:[5,7]},
+    
     /*Event CALLBACK/LISTENER + Bind  con Button */
     callBack_nuovoGioco : function (event )
     {
