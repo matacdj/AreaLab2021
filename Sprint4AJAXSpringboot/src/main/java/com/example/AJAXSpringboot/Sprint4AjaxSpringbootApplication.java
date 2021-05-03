3453f9c35eeba9e9b1ca8ed3e241cfaa4eeda1dc
Matteo Accordini <matteoaccordini18@gmail.com>
2021-05-03 11:30:23 +0200

    Upload progetto vuoto springboot e fix WebSpringDAO

---
+++ Sprint4AJAXSpringboot/src/main/java/com/example/AJAXSpringboot/Sprint4AjaxSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.AJAXSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint4AjaxSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint4AjaxSpringbootApplication.class, args);
+	}
+
+}
---
+++ Sprint4AJAXSpringboot/src/test/java/com/example/AJAXSpringboot/Sprint4AjaxSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AJAXSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint4AjaxSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
